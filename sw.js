e6e4de9318d3d3b444592c9aae17330107b59b68
XiaoYan <achwaljr32@gmail.com>
2026-05-15 02:44:23 +0700

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'chailin-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+             .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - network first, fallback cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
